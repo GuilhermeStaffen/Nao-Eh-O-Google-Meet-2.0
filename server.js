69078935e2c949643ee2237b32ff83f907ce86e4
Guilherme Staffen <guilherme@testingcompany.com.br>
2021-11-10 20:47:21 -0300

    login feature

--- server.js
+++ server.js
@@ -3,39 +3,42 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const port = process.env.PORT || 4000;
-const {v4:uuidv4} = require('uuid');
-const {ExpressPeerServer} = require('peer')
-const peer = ExpressPeerServer(server , {
-  debug:true
+const { v4: uuidv4 } = require('uuid');
+const { ExpressPeerServer } = require('peer')
+const peer = ExpressPeerServer(server, {
+  debug: true
 });
 
+
+app.use(express.static(__dirname + '/public'));
 app.use('/peerjs', peer);
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.get('/' , (req,res)=>{
-    res.render('login');
+
+app.get('/', (req, res) => {
+  res.render('login');
 });
 
-app.get('/meetCode' , (req,res)=>{
-  res.status(200).send({code: uuidv4()});
+app.get('/meetCode', (req, res) => {
+  res.status(200).send({ code: uuidv4() });
 });
 
-app.get('/mainScreen' , (req,res)=>{
-    res.render('main' , {UserName: req.params.userId});
+app.get('/mainScreen', (req, res) => {
+  res.render('main');
 });
 
-app.get('/room/:room' , (req,res)=>{
-    res.render('meet' , {RoomId:req.params.room});
+app.get('/:room', (req, res) => {
+  res.render('meet', { RoomId: req.params.room });
 });
-io.on("connection" , (socket)=>{
-  socket.on('newUser' , (id , room)=>{
+
+io.on("connection", (socket) => {
+  socket.on('newUser', (id, room) => {
     socket.join(room);
-    socket.to(room).broadcast.emit('userJoined' , id);
-    socket.on('disconnect' , ()=>{
-        socket.to(room).broadcast.emit('userDisconnect' , id);
+    socket.to(room).broadcast.emit('userJoined', id);
+    socket.on('disconnect', () => {
+      socket.to(room).broadcast.emit('userDisconnect', id);
     })
   })
 })
-server.listen(port , ()=>{
-  console.log("Server running on port : "+ port);
+server.listen(port, () => {
+  console.log("Server running on port : " + port);
 })
